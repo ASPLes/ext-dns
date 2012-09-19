4e7db5882fd9fcad47ee08ed4d1c01c5b9c706d4
Francis Brosnan Blázquez <francis@aspl.es>
2012-09-19 21:41:12 +0000

    ext-dns: * [fix] Some additional modifications to make ext-dnsd to properly communicate   with child processes. Now childs are created and an init command is send   to check the child talks ext-dns child communicate language..

--- server/child-resolver-skel.py
+++ server/child-resolver-skel.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
+import sys
+
 while True:
     # get command from ext-dns
-    command = raw_input ()
+    sys.stdout.flush ()
+    command = raw_input ().strip ()
+
+    # sys.stderr.write ("COMMAND RECEIVED: %s\n" % command)
 
     if command == "INIT":
+        # sys.stderr.write ("REPLYING INIT OK\n")
         # ext-dnsd is requesting to start this resolver, we have to
         # reply OK to satisfy its inquire anyway
-        print "OK"
+        print "OK\n"
     # end if
 
 # end while
