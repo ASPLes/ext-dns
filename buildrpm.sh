fad85ce1652f3235550ed2a410cdce7ccf8db5f5
Francis Brosnan Blázquez <francis@aspl.es>
2016-08-19 17:32:17 +0200

    ext-dns: * [fix] Updated buildrpm.sh to report error code reported   by rpmbuild when it fails..

--- buildrpm.sh
+++ buildrpm.sh
@@ -6,6 +6,7 @@ echo "Preparing sources"
 rm -rf rpm/SOURCES/*.tar.gz
 rm -rf rpm/BUILD/*
 rm -rf rpm/BUILDROOT/*
+test -d rpm/RPMS || mkdir -p rpm/RPMS
 find rpm/RPMS/ -type f -exec rm {} \;
 
 make dist
@@ -13,6 +14,14 @@ cp ext-dns-`cat VERSION`.tar.gz rpm/SOURCES/
 
 echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/ext-dns/rpm' rpm/SPECS/ext-dns.spec
+error=$?
+if [ $error != 0 ]; then
+    echo "ERROR: ***"
+    echo "ERROR: rpmbuild command failed, exitcode=$error"
+    echo "ERROR: ***"
+    exit $error
+fi
+
 
 echo "Output ready at rpm/RPMS"
 find rpm/RPMS -type f -name '*.rpm' > rpm/RPMS/files
